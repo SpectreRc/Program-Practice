4fa161e84dcbbeb7ff283ef6fddaeb0d0ea1a95e
A614\Admin <robertocg051096@gmail.com>
2024-10-17 21:05:53 +0200

    actu

--- tem2/src/main/java/org/example/Practica2u2.java
+++ tem2/src/main/java/org/example/Practica2u2.java
@@ -11,54 +11,67 @@ public class Practica2u2 {
 
         System.out.println("Bienvenido a la calculadora de Roberto.");
 
-        System.out.println("Introduce el primer número: ");
-        int num1 = entrada.nextInt();
+        boolean error = true;
+        int num1 = 0;
+        int num2 = 0;
+        boolean error2 = true;
+        double resultado;
+
+        while (error) {
+            try {
+                System.out.print("Introduce el primer número: ");
+                num1 = entrada.nextInt();
+                error = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Debes introducir un número válido.");
+                entrada.next();
+            }
+        }
 
         System.out.println("Introduce la operación que quieres hacer: (+,-,x,/,R) ");
         String operacion = entrada.next();
 
-        System.out.println("Introduce el segundo número: ");
-        int num2 = entrada.nextInt();
-
-        double resultado;
-
-        try{
-            switch (operacion){
-                case "+":
-                    resultado = num1 + num2;
-                    break;
-                case "-":
-                    resultado = num1 - num2;
-                    break;
-                case "x":
-                    resultado = num1 * num2;
-                    break;
-                case "/":
-                    if (num2 != 0) {
-                        resultado = (double) num1 / num2;
-                    } else {
-                        System.out.println("Error: División entre 0 no permitida.");
-                        return;
-                    }
-                    break;
-                case "R":
-                    if (num1 >= 0) {
-                        resultado = Math.sqrt(num1);
-                    } else {
-                        System.out.println("Error: No se puede calcular la raíz cuadrada de un número negativo.");
-                        return;
-                    }
-                    break;
-                default:
-                    System.out.println("Operación no válida.");
-                    return;
+        while (error2) {
+            try {
+                System.out.print("Introduce el segundo número: ");
+                num2 = entrada.nextInt();
+                error2 = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Debes introducir un número válido.");
+                entrada.next();
             }
-            System.out.println("El resultado de la operación es: " + resultado);
-
-        } catch (InputMismatchException e){
-            System.out.println("Error. ");
         }
 
+        switch (operacion){
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "x":
+                resultado = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0) {
+                    resultado = (double) num1 / num2;
+                } else {
+                    System.out.println("Error: División entre 0 no permitida.");
+                    return;
+                }
+                break;
+            case "R":
+                if (num1 >= 0) {
+                    resultado = Math.sqrt(num1);
+                } else {
+                    System.out.println("Error: No se puede calcular la raíz cuadrada de un número negativo.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Operación no válida.");
+                return;
+        }
+        System.out.println("El resultado de la operación es: " + resultado);
     }
-
 }
