006a305e88c0cb668139eff8b3b0594db32d7d28
A614\Admin <robertocg051096@gmail.com>
2024-10-23 19:45:20 +0200

    Practica3

--- tem2/src/main/java/org/example/Practica3u2.java
+++ tem2/src/main/java/org/example/Practica3u2.java
@@ -8,31 +8,48 @@ public class Practica3u2 {
 
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Bienvenido a la comprobante de ISBN de Roberto.");
-
-        int resultado = 0;
-
-        System.out.println("Introduzca el ISBN (10 números): ");
+        System.out.println("Introduce el ISBN (10 dígitos):");
         String isbn = entrada.nextLine();
 
         if (isbn.length() != 10) {
-            System.out.println("El ISBN debe tener exactamente 10 dígitos.");
+            System.out.println("El ISBN debe tener 10 caracteres.");
             return;
         }
 
-        try{
-            for (int i = 0; i < 10; i++){
-                int digito = Integer.parseInt(String.valueOf(isbn.charAt(i)));
-                resultado += digito * (10 - i);
+        if (isbn.contains("?")) {
+            int suma = 0;
+            int Interrogacion = isbn.indexOf('?');
+
+            for (int i = 0; i < 10; i++) {
+                if (i == Interrogacion) continue;
+                int valor = (isbn.charAt(i) == 'X') ? 10 : Character.getNumericValue(isbn.charAt(i));
+                suma += valor * (10 - i);
             }
 
-            if(resultado % 11 == 0) {
-                System.out.println("El ISBN es válido");
-            }else{
-                System.out.println("El ISBN no es válido");
-            }
-        }catch (StringIndexOutOfBoundsException e){
+            int peso = 10 - Interrogacion;
+            int digito = (11 - (suma % 11)) % 11;
 
+            if (digito == 10 && Interrogacion == 9) {
+                System.out.println("El dígito que falta es: X");
+            } else {
+                System.out.println("El dígito que falta es: " + digito);
+            }
+
+        } else {
+            int suma = 0;
+
+            for (int i = 0; i < 9; i++) {
+                suma += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
+            }
+
+            int ultimoDigito = isbn.charAt(9) == 'X' ? 10 : Character.getNumericValue(isbn.charAt(9));
+            suma += ultimoDigito;
+
+            if (suma % 11 == 0) {
+                System.out.println("El ISBN es válido.");
+            } else {
+                System.out.println("El ISBN no es válido.");
+            }
         }
     }
 }
